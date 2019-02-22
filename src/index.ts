9698a64c6767f543821729595d77187afb2ef39a
Kirill Shestakov <20163717+guitarino@users.noreply.github.com>
2019-02-22 01:36:33 -0800

    getting rid of duplicate exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import { Container as ContainerType } from './container.types';
-export * from './container';
 export * from './createImplementation';
 export * from './container';
 export type Container = ContainerType;
